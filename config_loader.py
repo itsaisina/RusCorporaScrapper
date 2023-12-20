39d024e08616e80f8060f0eac461d50b3e3fe028
Alina Aisina <shesaisina@gmail.com>
2023-12-20 13:56:53 +0300

    config_loader.py annotation

--- config_loader.py
+++ config_loader.py
@@ -1,6 +1,16 @@
 import json
+from typing import Any, Dict
 
 
-def load_config(path):
+def load_config(path: str) -> Dict[str, Any]:
+    """
+    Load a configuration from a JSON file.
+
+    Args:
+        path (str): The path to the JSON file to be loaded.
+
+    Returns:
+        Dict[str, Any]: A dictionary containing the parsed JSON data.
+    """
     with open(path, 'r') as file:
         return json.load(file)
