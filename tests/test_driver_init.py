d5955871201e72dbd58420494bad7808cdbfa597
pavel <pvlshkunov@gmail.com>
2023-12-23 02:01:24 +0300

    code style fixes

--- tests/test_driver_init.py
+++ tests/test_driver_init.py
@@ -1,6 +1,14 @@
-from driver_init import init_driver
+"""
+Tests for driver_init function
+"""
 from selenium import webdriver
+from driver_init import init_driver
 
 
 def test_driver_init():
+    """
+    Tests weather driver_init returns Chrome driver
+    Returns:
+
+    """
     assert isinstance(init_driver(), webdriver.Chrome)
