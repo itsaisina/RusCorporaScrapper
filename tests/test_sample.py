68d681d8581b162a6e72e2684608708626f4fc35
pavel <pvlshkunov@gmail.com>
2023-12-22 19:28:24 +0300

    update driver_init test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,10 +1,6 @@
 from driver_init import init_driver
+from selenium import webdriver
 
 
-def func(x):
-    return x + 1
-
-
-def test_answer():
-    import selenium
-    assert init_driver() != 'something'
+def test_driver_init():
+    assert isinstance(init_driver(), webdriver.Chrome)
