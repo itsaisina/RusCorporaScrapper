508a9286ebc35a9a8b5f74a719f5db9d9c058a72
pavel <pvlshkunov@gmail.com>
2023-12-23 04:03:02 +0300

    code style

--- tests/test_config_loader.py
+++ tests/test_config_loader.py
@@ -5,19 +5,18 @@ import json
 import pathlib
 from config.config_loader import load_config
 
-TEST_CONFIG_CONTENT = {"seed_url": "url", "x_paths": [{
-                                                          "search_input": "input",
-                                                          "word_elements": "element",
-                                                          "lemma": "lemma path",
-                                                          "grammar": "grammar path",
-                                                          "syntax_features_option":
-                                                              [
-                                                                  "opt 1",
-                                                                  "opt 2"
-                                                              ],
-                                                          "modal_close": "close",
-                                                          "next_page_button": "button"
-                                                      },], 'timeout': 15}
+TEST_CONFIG_CONTENT = dict(seed_url="url",
+                           x_paths=dict(search_input="input",
+                                        word_elements="element",
+                                        lemma="lemma path",
+                                        grammar="grammar path",
+                                        syntax_features_option=[
+                                            "opt 1",
+                                            "opt 2"
+                                                        ],
+                                        modal_close="close",
+                                        next_page_button="button"),
+                           timeout=15)
 
 FILE_PATH = pathlib.Path('test.json')
 
